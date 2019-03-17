bc04cc74371b96024d0d10132e03d1645672aabe
Nikolaos Antonakakis <nikolaos.antonakakis@wu.ac.at>
2019-03-17 09:43:18 +0100

    Detect images in AR

--- MagicPaper/ViewController.swift
+++ MagicPaper/ViewController.swift
@@ -23,18 +23,30 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
+//        // Create a new scene
+//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+//
+//        // Set the scene to the view
+//        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
+        // indicate what we will be tracking. In our case, an image, thus
+        // indicate that our configuration is tracking an Image
+        let configuration = ARImageTrackingConfiguration()
+        // check if the images we track are included in our bundle
+        if let trackedImages = ARReferenceImage.referenceImages(inGroupNamed: "NewsPaperImages", bundle: Bundle.main) {
+            // once we are certain that are tracked images we set them to be those specified above as trackedImages
+            configuration.trackingImages = trackedImages
+            // specify the max amount of images our configuration accepts
+            configuration.maximumNumberOfTrackedImages = 1
+            
+            
+
+        }
 
         // Run the view's session
         sceneView.session.run(configuration)
@@ -48,28 +60,28 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
 
     // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
+    // In order to receive the events from our scene when it detects a new image we need to add a delegate method
+    // anchor is the image that it finds; we need to return a node to be displayed in our scene
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        // first create a node
         let node = SCNNode()
-     
+        // check if this anchor is actually of the datatype image anchor
+        if let imageAnchor = anchor as? ARImageAnchor {
+            // create our plane
+            let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
+            // set its material to a new UIColor
+            plane.firstMaterial?.diffuse.contents = UIColor(white: 1.0, alpha: 0.5)
+            // create a plane node
+            let planeNode = SCNNode(geometry: plane)
+            // rotate the plane 90 degreess counterclockwise so that it's flat on the image
+            planeNode.eulerAngles.x = -.pi / 2
+            // tap into this node and add the child node that we created, i.e. planeNode
+            node.addChildNode(planeNode)
+            
+        }
+        
         return node
     }
-*/
     
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
+
 }
