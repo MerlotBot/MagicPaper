6cff7987c64dc7cd337070f5fd2ec0d783aeb249
Nikolaos Antonakakis <nikolaos.antonakakis@wu.ac.at>
2019-03-17 10:28:40 +0100

    Playing video in AR.

--- MagicPaper/ViewController.swift
+++ MagicPaper/ViewController.swift
@@ -67,14 +67,39 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         let node = SCNNode()
         // check if this anchor is actually of the datatype image anchor
         if let imageAnchor = anchor as? ARImageAnchor {
+            
+            // create a constant for the video node
+            let videoNode = SKVideoNode(fileNamed: "harrypotter.mp4")
+            
+            // let it start playing immediately
+            videoNode.play()
+            
+            // add the spritekit node to the scenekit node. To do that we need to create a new scene
+            let videoScene = SKScene(size: CGSize(width: 480, height: 360))
+            
+            // change the videoNode's position so that's centered
+            videoNode.position = CGPoint(x: videoScene.size.width/2, y: videoScene.size.height/2)
+            
+            // because it's flipped (upsidedown) we need to roatet it along the y axis so that we can see it the right way round
+            videoNode.yScale = -1.0
+            
+            // now add the video node to the videoscene
+            videoScene.addChild(videoNode)
+            
+            
+            
             // create our plane
             let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
-            // set its material to a new UIColor
-            plane.firstMaterial?.diffuse.contents = UIColor(white: 1.0, alpha: 0.5)
+            
+            // set its material to a new UIColor; then we change it to videoScene as the object to cover our plane
+            plane.firstMaterial?.diffuse.contents = videoScene //UIColor(white: 1.0, alpha: 0.5)
+            
             // create a plane node
             let planeNode = SCNNode(geometry: plane)
+            
             // rotate the plane 90 degreess counterclockwise so that it's flat on the image
             planeNode.eulerAngles.x = -.pi / 2
+            
             // tap into this node and add the child node that we created, i.e. planeNode
             node.addChildNode(planeNode)
             
